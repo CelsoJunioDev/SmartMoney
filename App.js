fc8e6e1b04f5f4d6241b1b8520ba54729fcea4ca
CelsoJunioDev <celsoj18@gmail.com>
2020-10-06 13:01:51 -0300

    Montagem do esqueleto do app

--- App.js
+++ App.js
@@ -13,102 +13,57 @@ import {
   ScrollView,
   View,
   Text,
+  Button,
+  FlatList,
   StatusBar,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+const App = () => {
+  addEntry = () =>{
+    alert("Abrir add lançamento");
+  }
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step 123456</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style= {{padding: 10}}>
+      <Text 
+      style={{fontSize: 22, fontWeight: "bold", marginTop: 10, marginBottom: 10}}>
+        Saldo atual: $2.102,45
+        </Text>
+        <Button 
+        onPress={addEntry} 
+        title="Adicionar"></Button>
+
+        <Text style={{fontSize: 22, fontWeight: "bold", 
+        marginTop: 10, marginBottom: 10}}>Categorias</Text>
+
+  <FlatList
+    data={[
+      {key: "Aliamentação: $200" },
+      {key: "Combustivel: $12" },
+      {key: "Aluguel: $120" },
+      {key: "Lazer: $250" },
+      {key: "Outros: $1200" }      
+    ]}
+    renderItem={({item}) => <Text>{item.key}</Text>}>
+  </FlatList>
+  <Text style={{fontSize: 22, fontWeight: "bold", 
+  marginTop: 10, marginBottom: 10}}>Ultimos lançamentos</Text>
+
+  <FlatList
+    data={[
+      {key: "Padaria asa branca: $10" },
+      {key: "Supermercado Isadora: $190" },
+      {key: "Posto Ipiranga: $190" }     
+    ]}
+    renderItem={({item}) => <Text>{item.key}</Text>}>
+  </FlatList>
+        
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
 });
 
 export default App;
